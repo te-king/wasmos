a644b45775bc9952d5cef932917f7981ec74c97a
Thomas King <thomas.e.king@icloud.com>
2023-07-12 19:20:56 +1000

    Read and print out the UEFI memory map. This memory map will be used for creating an allocator.

--- crates/wasmos/src/main.rs
+++ crates/wasmos/src/main.rs
@@ -4,7 +4,9 @@
 
 extern crate alloc;
 
-use uefi::prelude::*;
+use core::slice;
+
+use uefi::{prelude::*, table::boot::MemoryType};
 use uefi_services::println;
 use wasmi::{Caller, Engine, Func, Linker, Module, Store};
 
@@ -13,6 +15,26 @@ const WSHELL: &[u8] = include_bytes!(env!("CARGO_BIN_FILE_WSHELL"));
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
+
+    let memory_map = unsafe {
+        // Create a buffer for the memory map
+        let buffer_size = system_table.boot_services().memory_map_size().map_size;
+        let buffer_raw = system_table
+            .boot_services()
+            .allocate_pool(MemoryType::LOADER_DATA, buffer_size)
+            .unwrap();
+        let buffer_slice = slice::from_raw_parts_mut(buffer_raw as *mut u8, buffer_size);
+
+        // Get the memory map
+        system_table
+            .boot_services()
+            .memory_map(buffer_slice)
+            .unwrap()
+    };
+
+    for desc in memory_map.entries() {
+        println!("{:?}", desc);
+    }
 
     let engine = Engine::default();
     let mut linker = <Linker<()>>::new(&engine);
