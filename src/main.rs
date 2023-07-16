370257030b0841bcabf91e89493bd368d29150e0
Thomas King <thomas.e.king@icloud.com>
2023-07-16 11:17:24 +1000

    flattened kernel crate up one level

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,23 @@
 
 extern crate alloc;
 
-use core::slice;
+use core::{future::Future, pin::Pin};
 
-use uefi::{prelude::*, table::boot::MemoryType};
+use uefi::prelude::*;
 use uefi_services::println;
 use wasmi::{Caller, Engine, Func, Linker, Module, Store};
+
+mod qemu;
 
 const WSHELL: &[u8] = include_bytes!(env!("CARGO_BIN_FILE_WSHELL"));
 
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    // retrieve memory map, exit boot services, and add to allocator
+    // let (mut system_table, memory_map) = system_table //
+    // .exit_boot_services();
+
     uefi_services::init(&mut system_table).unwrap();
-
-    let memory_map = unsafe {
-        // Create a buffer for the memory map
-        let buffer_size = system_table.boot_services().memory_map_size().map_size;
-        let buffer_raw = system_table
-            .boot_services()
-            .allocate_pool(MemoryType::LOADER_DATA, buffer_size)
-            .unwrap();
-        let buffer_slice = slice::from_raw_parts_mut(buffer_raw as *mut u8, buffer_size);
-
-        // Get the memory map
-        system_table
-            .boot_services()
-            .memory_map(buffer_slice)
-            .unwrap()
-    };
-
-    for desc in memory_map.entries() {
-        println!("{:?}", desc);
-    }
 
     let engine = Engine::default();
     let mut linker = <Linker<()>>::new(&engine);
@@ -88,7 +74,17 @@ fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     let hello = instance.get_typed_func::<(), ()>(&store, "main").unwrap();
     hello.call(&mut store, ()).unwrap();
 
-    loop {}
-
+    qemu::exit_qemu(qemu::QemuExitCode::Success);
     Status::SUCCESS
+}
+
+/// A task that will be preempted after a certain amount of time,
+/// if it is not completed before then.
+/// Execution will execute within a different stack frame.
+struct PreemptiveTask;
+
+impl Future<Output = ()> for PreemptableTask {
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        Poll::Ready(())
+    }
 }
